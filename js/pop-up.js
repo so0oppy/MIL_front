0aad52353adc41498103e178457121238e4704bf
Subin <80036437+hansb1023@users.noreply.github.com>
2023-01-18 00:08:54 +0900

    팝업 추가

--- js/pop-up.js
+++ js/pop-up.js
@@ -5,3 +5,27 @@ function open_pop_add(flag){
 function close_pop_add(flag){
     $('#Modal_add').hide();
 };
+
+function open_pop_subject(flag){
+    $('#Modal_add').css('display', 'block');
+};
+
+// function close_pop_subject(flag){
+//     $('#Modal_add').hide();
+// };
+
+function open_pop_modify(flag){
+    $('#Modal_add').css('display', 'block');
+};
+
+// function close_pop_modify(flag){
+//     $('#Modal_add').hide();
+// };
+
+function open_pop_enter(flag){
+    $('#Modal_add').css('display', 'block');
+};
+
+// function close_pop_modify(flag){
+//     $('#Modal_add').hide();
+// };
