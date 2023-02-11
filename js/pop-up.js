6ee8772ed1c869bfd8595ad5c7d636f01d036d6e
Subin <80036437+hansb1023@users.noreply.github.com>
2023-02-11 17:13:09 +0900

    워드프레스 스크립트 적용 (자체 스크립트로 변경 예정)

--- js/pop-up.js
+++ js/pop-up.js
@@ -29,3 +29,58 @@ function open_pop_enter(flag){
 function close_pop_enter(flag){
     $('#Modal_enter').hide();
 };
+
+
+function trackClick(page_id, job_id){
+    var ch_td;
+    var core = new Array();
+    var support = new Array();
+    var url = "../curriculum.html";
+    reset_color();
+
+    $.ajax({
+            type: 'POST',
+			url: url,
+			async: true,
+			data: {"page_id" : page_id},
+			dataType : 'json',
+			success: function (data) {
+				for(var i=0; i<data.subjectTrackList.length; i++)
+				{
+					if(data.subjectTrackList[i].type=='core')
+					{
+						core.push('tr'+data.subjectTrackList[i].row_id+'td'+data.subjectTrackList[i].col_id);
+					}
+					if(data.subjectTrackList[i].type=='support')
+					{
+						support.push('tr'+data.subjectTrackList[i].row_id+'td'+data.subjectTrackList[i].col_id);
+					}
+				}
+				switch(page_id)
+				{
+					case 129:
+						deClick(core,support);
+						break;
+					case 130:
+						cdClick(core,support);
+						break;
+					case 131:
+						vcClick(core,support);
+						break;
+					case 132:
+						mdClick(core,support);
+						break;
+					case 133:
+						giClick(core,support);
+						break;
+				}
+			},
+			complete: function() {
+			},
+			error:function(request, status, error){
+				alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+                console.log("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+
+			}
+    });
+}
